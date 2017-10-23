e08c29c856a323de3c27f111f132847b4a623578
Ian Moore <18MooreI@apccurr.local>
2017-10-23 07:27:43 -0400

    Add prototype autonomous
    In the current state, this untested autonomous would probably
    destroy the bot if ran. There are no sensors currently programmed
    in and the directions may also be wrong. This is just to get
    the basic idea down so I have something to work with when I
    finally get a chance to work with the bot

--- src/auto.c
+++ src/auto.c
@@ -6,7 +6,33 @@ task autonomous(){
 	//Checks if autonomous is enabled and should run
 	//Don't run autonomous if the power expander has no battery, so we can touch it and replace it
 	if(SensorValue[autonomousToggle]||!SensorValue[powerExpander]){return;}
+	//Drive backwards to mobile goal
 	drive(-127);
-	wait(3);
+	wait(3); //TODO: Use some sensor to determine how far to drive
 	drive(0);
+	//Pickup mobile goal
+	pistons(1);
+	wait(1);
+	//Slam preload on top
+	arms(-127);
+	wait(2); //TODO: Use encoder to determine how far to move
+	intake(-127);
+	wait(.5);
+	arms(127);
+	wait(1);
+	arms(ARM_LOCK); //Lock the arms in air so they don't get in the way
+	//Drive back to the start
+	drive(127);
+	intake(0);
+	wait(2);
+	drive(127, 127); //Turn around
+	wait(1);
+	drive(127);
+	wait(2);
+	pistons(0);
+	drive(-127);
+	wait(2);
+	//Stop
+	drive(0);
+	arms(0);
 }
