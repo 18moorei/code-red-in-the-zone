37980ede9813a4d333fcd4e482b924c5adcf92cc
Ian Moore <18MooreI@apccurr.local>
2017-10-24 07:36:32 -0400

    Add mirrored partner controller support

--- src/user.c
+++ src/user.c
@@ -1,29 +1,31 @@
+#include "user.h"
+
 task usercontrol(){
 	int DY, DT;
 	bool armsLocked = false;
 	while(true){
 		//Driving
-		DY = threshold(vexRT[Ch2], 15);
-		DT = threshold(vexRT[Ch1], 15);
+		DY = threshold(PAIRED_CH2, 15);
+		DT = threshold(PAIRED_CH1, 15);
 		drive(DY, DT);
 		//Pistons (toggle)
-		if(vexRT[Btn7R]){
+		if(PAIRED_BTN7R){
 			pistons(!PISTON_POS);
-			waitUntil(!vexRT[Btn7R]);
+			waitUntil(!PAIRED_BTN7R);
 		}
 		//Arms
-		if(vexRT[Btn7L]){
+		if(PAIRED_BTN7L){
 			armsLocked = !armsLocked;
-			waitUntil(!vexRT[Btn7L]);
+			waitUntil(!PAIRED_BTN7L);
 		}
 		if(armsLocked){
 			arms(ARM_LOCK);
 		} else {
-			arms((vexRT[Btn6U] - vexRT[Btn6D]) * MAX_POWER);
+			arms((PAIRED_BTN6U - PAIRED_BTN6D) * MAX_POWER);
 		}
 		//Lift
-		lift((vexRT[Btn7U] - vexRT[Btn7D]) * MAX_POWER);
+		lift((PAIRED_BTN7U - PAIRED_BTN7D) * MAX_POWER);
 		//Intake
-		intake((vexRT[Btn5U] - vexRT[Btn5D]) * MAX_POWER);
+		intake((PAIRED_BTN5U - PAIRED_BTN5D) * MAX_POWER);
 	}
 }
