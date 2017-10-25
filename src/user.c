cbf98a2d973a63b9c4e0e81fd8f8fa442c5c5186
Ian Moore <18moorei@APCCURR.LOCAL>
2017-10-25 07:34:38 -0400

    Add whitespace for readability

--- src/user.c
+++ src/user.c
@@ -1,16 +1,19 @@
 task usercontrol(){
 	int DY, DT;
 	bool armsLocked = false;
+
 	while(true){
 		//Driving
 		DY = threshold(PAIRED_CH2, 15);
 		DT = threshold(PAIRED_CH1, 15);
 		drive(DY, DT);
+
 		//Pistons (toggle)
 		if(PAIRED_BTN7R){
 			pistons(!PISTON_POS);
 			waitUntil(!PAIRED_BTN7R);
 		}
+
 		//Arms
 		if(PAIRED_BTN7L){
 			armsLocked = !armsLocked;
@@ -21,8 +24,10 @@ task usercontrol(){
 		} else {
 			arms((PAIRED_BTN6U - PAIRED_BTN6D) * MAX_POWER);
 		}
+
 		//Lift
 		lift((PAIRED_BTN7U - PAIRED_BTN7D) * MAX_POWER);
+
 		//Intake
 		intake((PAIRED_BTN5U - PAIRED_BTN5D) * MAX_POWER);
 	}
