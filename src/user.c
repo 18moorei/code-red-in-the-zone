6d640c7ea2777984c86c0b5e1e794c9c6dae6aa9
18moorei <18moorei@apcsd.org>
2017-11-13 07:36:18 -0500

    Make drive joystick exponental
    This way people can drive slower easier

--- src/user.c
+++ src/user.c
@@ -9,8 +9,8 @@ task usercontrol(){
 
 	while(true){
 		//Driving
-		DY = threshold(PAIRED_CH2, 15) + (PAIRED_BTN8U * MAX_POWER) - (PAIRED_BTN8D * MAX_POWER);
-		DT = threshold(PAIRED_CH1, 15) + (PAIRED_BTN8R * MAX_POWER) - (PAIRED_BTN8L * MAX_POWER);
+		DY = threshold(PAIRED_CH2^2 / MAX_POWER, 5) + ((PAIRED_BTN8U - PAIRED_BTN8D) * MAX_POWER);
+		DT = threshold(PAIRED_CH1^2 / MAX_POWER, 5) + ((PAIRED_BTN8R - PAIRED_BTN8L) * MAX_POWER);
 		drive(DY, DT);
 
 		//Pistons (toggle)
