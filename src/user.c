4d15a6acab8ecffa5112722224e3b79fa5dc5d47
18moorei <18moorei@apcsd.org>
2017-12-13 07:33:59 -0500

    Change motor configuration
    Reduce mobile goal intake motors to one
    Add split for the scissor lift
    Remove the cente wheel

--- src/user.c
+++ src/user.c
@@ -1,12 +1,11 @@
 task usercontrol(){
-	int DY, DT, DX;
+	int DY, DT;
 
 	while(true){
 		//Driving
 		DY = threshold(PAIRED_CH2^2 / MAX_POWER, 5) + ((PAIRED_BTN8U - PAIRED_BTN8D) * MAX_POWER);
 		DT = threshold(PAIRED_CH1^2 / MAX_POWER, 5);
-		DX = threshold(PAIRED_CH4^2 / MAX_POWER, 5) + ((PAIRED_BTN8R - PAIRED_BTN8L) * MAX_POWER);
-		drive(DY, DT, DX);
+		drive(DY, DT);
 
 		//Mogo
 		mogo(threshold(PAIRED_CH3, 15));
