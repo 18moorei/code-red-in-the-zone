db81f44005db38e6e42865d0a27c2ff91b0bf700
18moorei <18moorei@apcsd.org>
2018-01-10 10:26:57 -0500

    Improve position calculations
    Add biases

--- src/pos.c
+++ src/pos.c
@@ -1,20 +1,25 @@
 typedef struct {
 	int x;
 	int y;
+	int r;
 } Point;
 
 Point current_pos;
 
+int aclY_bias;
+
 task positionsystem {
+	int velY = 0, dir, c, a, b;
 	while (true) {
-		int velX = threshold(SensorValue[aclX], 1);
-		int velY = threshold(SensorValue[aclY], 1);
-		int dir = SensorValue[gyro] % 360;
-		int c = velY;
-		int a = cos(dir) * c; //x
-		int b = sin(dir) * c; //y
+		velY = threshold(SensorValue[aclY], aclY_bias);
+		dir = SensorValue[gyro] % 3600;
+		c = velY;
+		a = cos(dir) * c; //x
+		b = sin(dir) * c; //y
 		current_pos.x += a;
 		current_pos.y += b;
+		current_pos.r = dir;
+		wait1Msec(1000);
 	}
 }
 
@@ -22,7 +27,11 @@ void setHome(void) {
 	current_pos.x = 0;
 	current_pos.y = 0;
 	SensorValue[gyro] = 0;
+	SensorScale[gyro] = 260;
+	SensorFullCount[gyro] = 3600;
 	SensorValue[aclX] = 0;
 	SensorValue[aclY] = 0;
 	SensorValue[aclZ] = 0;
+	wait1Msec(1000);
+	aclY_bias = SensorValue[aclY] + 1;
 }
